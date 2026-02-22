4d853c8d8f87aef427910df285bebfdff28eb596
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-02-21 23:13:09 -0300

    Configure menu bar app and OpenKubbo top bar icon

--- OpenKubbo/OpenKubboApp.swift
+++ OpenKubbo/OpenKubboApp.swift
@@ -6,12 +6,31 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct OpenKubboApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra {
+            VStack(alignment: .leading, spacing: 12) {
+                ContentView()
+
+                Divider()
+
+                Button("Quit OpenKubbo") {
+                    NSApplication.shared.terminate(nil)
+                }
+            }
+            .padding(12)
+            .frame(width: 220)
+        } label: {
+            Image("OpenKubbo")
+                .renderingMode(.template)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 16, height: 16)
+                .accessibilityLabel("OpenKubbo")
         }
+        .menuBarExtraStyle(.window)
     }
 }
